66b530d3e8770bfc5444fc7b40a49d510797ecc8
Erik <drummerik@gmail.com>
2021-06-28 13:24:37 +0200

    added files

--- site.js
+++ site.js
@@ -1,5 +1,6 @@
 console.log("Hellow World!")
 console.log("Added another Hello World!")
 console.log("Added really another one!");
+const nr = Number.POSITIVE_INFINITY;
 
-alert("whats going on?");
+alert("whats going on with number: ", nr);
